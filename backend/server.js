7560de7c1b9c36cd8116268ab134a9051754496e
Somesh <someshverma8055@gmail.com>
2024-01-28 14:53:54 +0530

    bcrypt

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 dotenv.config();
 connectDB();
@@ -9,12 +10,15 @@ const app = express();
 
 app.use(express.json())
 
-const PORT = process.env.PORT || 4000;
-
 app.use('/api/user', userRoutes)
 
 app.get("/", (req, res) => {
     res.send("API is running");
 })
+
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, console.log(`Server Started on PORT ${PORT}`))
